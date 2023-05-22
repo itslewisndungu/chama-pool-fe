4d890b9b8252780940cbb2dadfa1d49b8a35eab5
itslewisndungu <itslewisndungu@gmail.com>
2023-05-22 22:00:08 +0300

    feat: Link up  the `members` pages

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,51 +1,45 @@
 'use client';
 
-import { Navbar, Group, Code, ScrollArea, createStyles, rem } from '@mantine/core';
-import {
-  IconNotes,
-  IconCalendarStats,
-  IconGauge,
-  IconPresentationAnalytics,
-  IconFileAnalytics,
-  IconAdjustments,
-  IconLock
-} from '@tabler/icons-react';
+import { Navbar, ScrollArea, createStyles } from '@mantine/core';
+import { IconGauge, IconUsers } from '@tabler/icons-react';
 import { LinksGroup } from '@/components/LinkGroups';
 
 const mockdata = [
   {
-    label: 'Dashboard', icon: IconGauge, link: '/dashboard'
+    label: 'Dashboard',
+    icon: IconGauge,
+    link: '/dashboard',
   },
   {
-    label: 'Market news',
-    icon: IconNotes,
-    links: [
-      { label: 'Overview', link: '/' },
-      { label: 'Forecasts', link: '/' },
-      { label: 'Outlook', link: '/' },
-      { label: 'Real time', link: '/' }
-    ]
-  }
+    label: 'Members',
+    icon: IconUsers,
+    links: [{ label: 'Invite new member', link: '/members/new-member' }],
+  },
 ];
 
-const useStyles = createStyles((theme) => ({
+const useStyles = createStyles(theme => ({
   navbar: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
-    paddingBottom: 0
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
   },
 
   links: {
     marginLeft: `calc(${theme.spacing.md} * -1)`,
-    marginRight: `calc(${theme.spacing.md} * -1)`
-  }
+    marginRight: `calc(${theme.spacing.md} * -1)`,
+  },
 }));
 
 export function Sidebar() {
   const { classes } = useStyles();
-  const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
+  const links = mockdata.map(item => <LinksGroup {...item} key={item.label} />);
 
   return (
-    <Navbar height={800} width={{ sm: 300 }} p='md' className={classes.navbar}>
+    <Navbar
+      width={{ sm: 300 }}
+      p="md"
+      className={`${classes.navbar} min-h-full`}
+    >
       <Navbar.Section grow className={classes.links} component={ScrollArea}>
         <div className={''}>{links}</div>
       </Navbar.Section>
