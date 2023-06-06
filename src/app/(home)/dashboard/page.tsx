6420db518ae68dd61c6241846aba8633dcd90968
itslewisndungu <itslewisndungu@gmail.com>
2023-06-06 12:26:48 +0300

    feat: Create sample financial summary stats

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-  return <h1>Dashboard page</h1>
+import { GroupFinancialSummary } from '@/components/GroupFinancialSummary';
+
+const getData = async () => {
+  const data = [
+    // {
+    //   title: 'Account Balance',
+    //   value: '13,456',
+    //   diff: 34,
+    //   icon: 'coin',
+    // },
+    {
+      title: 'Mothly Revenue',
+      value: '4,145',
+      icon: 'receipt',
+      diff: -13,
+    },
+    {
+      title: 'Montly Expenses',
+      icon: 'expense',
+      value: '745',
+      diff: 18,
+    },
+  ];
+
+  return data;
+};
+export default async function Page() {
+  const data = await getData();
+
+  return (
+    <>
+      <p className="text-xl font-bold m-0 text-gray-700">Financial Summary</p>
+      <GroupFinancialSummary data={data} />
+    </>
+  );
 }
