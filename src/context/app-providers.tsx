81f07621b3ec2efa800a61247c264c5f2c1794b8
itslewisndungu <itslewisndungu@gmail.com>
2023-06-15 16:07:42 +0300

    Remove unused Providers HOC

--- src/context/app-providers.tsx
+++
@@ -1,8 +0,0 @@
-import RootStyleRegistry from '@/app/emotion';
-import { AuthProvider } from './auth-context';
-
-const AppProviders = ({ children }: { children: React.ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>;
-};
-
-export default AppProviders;
