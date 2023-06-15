7df75c9a15dd690d87491035ed4ff5a17ffbb752
itslewisndungu <itslewisndungu@gmail.com>
2023-06-15 16:08:24 +0300

    Remove `setLoading` in favor of state enum

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { getCurrentUser, userLogin } from '@/lib/api/user';
-import { LoginCredentials } from '@/types/LoginCredentials';
-import { User } from '@/types/User';
-import { useRouter } from 'next/navigation';
+import { getCurrentUser, userLogin } from "@/lib/api/user";
+import { LoginCredentials } from "@/types/LoginCredentials";
+import { User } from "@/types/User";
+import { useRouter } from "next/navigation";
 import {
   createContext,
   useCallback,
@@ -11,15 +11,14 @@ import {
   useMemo,
   useState,
   FC,
-} from 'react';
+} from "react";
 
 interface AuthContextType {
   user?: User;
   token?: string;
-  state: 'loading' | 'loggedOut' | 'loggedIn';
+  state: "loading" | "loggedOut" | "loggedIn";
   error: string | null;
   setUser: (user: User) => void;
-  setLoading: (isLoading: boolean) => void;
   setToken: (token: string) => void;
   login: (credentials: LoginCredentials) => void;
   logout: () => void;
@@ -33,70 +32,66 @@ export const AuthContext = createContext<AuthContextType>(
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User>();
   const [error, setError] = useState<string | null>(null);
-  const [state, setState] = useState<'loading' | 'loggedIn' | 'loggedOut'>(
-    'loggedOut'
+  const [state, setState] = useState<"loading" | "loggedIn" | "loggedOut">(
+    "loggedOut"
   );
   const [token, setToken] = useState<string>();
   const router = useRouter();
 
   useEffect(() => {
     // Run on page load when a user has not logged out
-    setState('loading');
-    const token = localStorage.getItem('token');
+    setState("loading");
+    const token = localStorage.getItem("token");
 
     if (token === null) {
-      setState('loggedOut');
+      setState("loggedOut");
       return;
     }
 
     async function fetchUser(token: string) {
-      const res: { member: User; token: string } = await getCurrentUser(
-        token
-      ).then(res => res.json());
-      setState('loggedIn');
-      setUser(res.member);
-      setToken(res.token);
+      try {
+        const res: { member: User; token: string } = await getCurrentUser(
+          token
+        ).then(res => res.json());
+
+        setState("loggedIn");
+        setUser(res.member);
+        setToken(res.token);
+      } catch (e) {
+        setError("Credentials have expired. Please log in again");
+        setToken(undefined);
+      }
     }
+
+    fetchUser(token);
+  }, []);
+
+  const login = useCallback(async (credentials: LoginCredentials) => {
+    setState("loading");
+    setError(null);
 
     try {
-      fetchUser(token);
-      router.push('/dashboard');
+      const res = await userLogin(credentials);
+      const { member, token }: { member: User; token: string } =
+        await res.json();
+
+      setUser(member);
+      setToken(token);
+      setState("loggedIn");
+      localStorage.setItem("token", token);
     } catch (e) {
-      setToken(undefined);
+      setError("Invalid username or password");
+      setState("loggedOut");
     }
-  }, [router]);
-
-  const login = useCallback(
-    async (credentials: LoginCredentials) => {
-      setState('loading');
-      setError(null);
-
-      try {
-        const res = await userLogin(credentials);
-        const { member, token }: { member: User; token: string } =
-          await res.json();
-
-        setUser(member);
-        setToken(token);
-        setState('loggedIn');
-        localStorage.setItem('token', token);
-
-        router.push('/dashboard');
-      } catch (e) {
-        setError('Invalid username or password');
-        setState('loggedOut');
-      }
-    },
-    [router]
-  );
+  }, []);
 
   const logout = useCallback(async () => {
-    setState('loading');
+    setState("loading");
     setUser(undefined);
     setToken(undefined);
-    localStorage.removeItem('token');
-    setState('loggedOut');
-    router.push('/login');
+    localStorage.removeItem("token");
+    setState("loggedOut");
+    router.push("/login");
   }, [router]);
 
   const memoedValue = useMemo(
