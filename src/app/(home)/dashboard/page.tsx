74155eb81a98788d6ed01cbac8dfed04ef91f586
itslewisndungu <itslewisndungu@gmail.com>
2023-06-16 10:49:45 +0300

    format: Use double quotes instead of single quotes

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import { GroupFinancialSummary } from '@/components/GroupFinancialSummary';
+import { GroupFinancialSummary } from "@/components/GroupFinancialSummary";
 
 const getData = async () => {
-  const data = [
+  return [
     // {
     //   title: 'Account Balance',
     //   value: '13,456',
@@ -9,27 +9,25 @@ const getData = async () => {
     //   icon: 'coin',
     // },
     {
-      title: 'Mothly Revenue',
-      value: '4,145',
-      icon: 'receipt',
+      title: "Monthly Revenue",
+      value: "4,145",
+      icon: "receipt",
       diff: -13,
     },
     {
-      title: 'Montly Expenses',
-      icon: 'expense',
-      value: '745',
+      title: "Monthly Expenses",
+      icon: "expense",
+      value: "745",
       diff: 18,
     },
   ];
-
-  return data;
 };
 export default async function Page() {
   const data = await getData();
 
   return (
     <>
-      <p className="text-xl font-bold m-0 text-gray-700">Financial Summary</p>
+      <h1 className="text-xl font-bold m-0 text-gray-700">Financial Summary</h1>
       <GroupFinancialSummary data={data} />
     </>
   );
