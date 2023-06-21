e5c11176751004e8f8e629c175914a2933a02385
itslewisndungu <itslewisndungu@gmail.com>
2023-06-21 11:16:13 +0300

    refactor: Move `getFormattedCurrency` to `utils.ts` file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
-const getFormattedCurrency = (amount: number) => {
+import { keys } from "@mantine/utils";
+
+export const getFormattedCurrency = (amount: number) => {
   return new Intl.NumberFormat("en-KE", {
     style: "currency",
     currency: "KSH",
   }).format(amount);
 };
-
-export default getFormattedCurrency;
