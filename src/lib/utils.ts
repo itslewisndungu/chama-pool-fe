c7881456593febf3a59051aa44dba009b0abd741
itslewisndungu <itslewisndungu@gmail.com>
2023-06-21 11:16:51 +0300

    Add table sorting and filtering util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,55 @@ export const getFormattedCurrency = (amount: number) => {
     currency: "KSH",
   }).format(amount);
 };
+
+const filterTableData = <T>(data: T[], search: string) => {
+  const query = search.toLowerCase().trim().split(",");
+
+  return data.filter(item =>
+    keys(data[0]).some(key => {
+      return query.some(term =>
+        String(item[key]).toLowerCase().includes(term.trim())
+      );
+    })
+  );
+};
+
+const compareTableValues = (value1: unknown, value2: unknown) => {
+  // Handle undefined values
+  if (value1 === undefined && value2 === undefined) {
+    return 0;
+  } else if (value1 === undefined) {
+    return 1;
+  } else if (value2 === undefined) {
+    return -1;
+  }
+
+  // Handle number values
+  if (typeof value1 === "number" && typeof value2 === "number") {
+    return value1 - value2;
+  }
+
+  // Fallback to string comparison
+  return String(value1).localeCompare(String(value2));
+};
+
+export const sortTableData = <T>(
+  data: T[],
+  payload: { sortBy: keyof T | null; reversed: boolean; search: string }
+) => {
+  const { sortBy } = payload;
+
+  if (!sortBy) {
+    return filterTableData<T>(data, payload.search);
+  }
+
+  return filterTableData<T>(
+    [...data].sort((a, b) => {
+      if (payload.reversed) {
+        return compareTableValues(b[sortBy], a[sortBy]);
+      }
+      return compareTableValues(a[sortBy], b[sortBy]);
+    }),
+    payload.search
+  );
+};
