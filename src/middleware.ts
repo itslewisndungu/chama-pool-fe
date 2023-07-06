35cc23b68d0403b9e32fa8bb115f5941d0df578b
itslewisndungu <itslewisndungu@gmail.com>
2023-07-06 08:11:46 +0300

    fix(TS): Type the reqForNextAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,32 +2,33 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getSession } from "next-auth/react";
 import { MemberRole } from "./types/MemberRole";
+import { IncomingMessage } from "http";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const requestForNextAuth = {
+  const requestForNextAuth: Partial<IncomingMessage> = {
     headers: {
-      cookie: request.headers.get("cookie"),
+      cookie: request.headers.get("cookie") ?? undefined,
     },
   };
 
   const session = await getSession({ req: requestForNextAuth });
 
   if (!session) {
-      return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   const isAdmin = session.user.roles.some(
-      role => role === MemberRole.SECRETARY || role === MemberRole.TREASURER || role === MemberRole.CHAIRMAN
-  )
-
-  console.log({isAdmin, role: session.user.roles})
+    role =>
+      role === MemberRole.SECRETARY ||
+      role === MemberRole.TREASURER ||
+      role === MemberRole.CHAIRMAN
+  );
 
   if (isAdmin) {
-      return NextResponse.redirect(new URL('/group/dashboard', request.url))
+    return NextResponse.redirect(new URL("/group/dashboard", request.url));
   }
 
-  return NextResponse.redirect(new URL('/member/dashboard', request.url))
+  return NextResponse.redirect(new URL("/member/dashboard", request.url));
 }
 
 // See "Matching Paths" below to learn more
